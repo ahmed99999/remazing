1a49d4ae7aa7b5977f17e4c89e9420a2a0fe6166
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-09-10 12:19:11 +0200

    DEV01: adding the clients component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import Clients from './components/Clients';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Switch>
-      {/* <Route path="/questions/:id" component={QuestionDetails} />
-      <Route path="/questions" component={Questions} />
-      <Redirect from="/" exact to="/questions" /> */}
+      <Route path="/clients" component={Clients} />
+      <Redirect from="/" exact to="/clients" />
     </Switch>
   );
 }
---
+++ src/components/Clients/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import classes from './clients.module.scss';
+
+class Clients extends Component {
+    state = {}
+    render() {
+        return (
+            <div className={classes.clients}>
+                Clients
+            </div>
+        );
+    }
+}
+
+export default Clients;
