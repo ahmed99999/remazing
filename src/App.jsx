3e8d70fe8ab460b1f1ea06802cc5331cc2837607
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-09-10 11:28:12 +0200

    master: project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <Switch>
+      {/* <Route path="/questions/:id" component={QuestionDetails} />
+      <Route path="/questions" component={Questions} />
+      <Redirect from="/" exact to="/questions" /> */}
+    </Switch>
+  );
+}
+
+export default App;
