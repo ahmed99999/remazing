2e56a06fa93bfb07a583ef336e622a21dcd2abc4
Ahmed Amin Bakhabou <AhmedaminHD@outlook.com>
2020-09-14 13:03:38 +0200

    Dashborad Component: first initial commit

---
+++ src/components/common/Dashborad/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './dashborad.module.scss';
+
+const Dashborad = () => {
+    return (
+        <div className={classes.dashborad}>
+            Dashborad
+        </div>
+    );
+}
+
+export default Dashborad;
